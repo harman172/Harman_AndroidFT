1eaaad3f241dbd383f050c170381cbb2b5d605fb
Harman Kaur <kaurharmanpreet502@gmail.com>
2019-12-13 11:09:15 -0500

    Registration activity and layout

---
+++ app/src/main/java/com/example/harman_c0765590_ft/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.example.harman_c0765590_ft;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
